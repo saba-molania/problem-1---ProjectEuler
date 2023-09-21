8063accac18b3c9b3ba2e6738d35a2237e5d2fcc
Saba <saba.molania51@gmail.com>
2023-09-21 22:24:54 +0330

    refactor timer

--- 02/timer.py
+++ 02/timer.py
@@ -1,25 +1,48 @@
 import time
 from os import system,name
-while True :
-    choice = input("\nDo you want to start? (y/n) :\t")
-    if "y" in choice.lower() :
-        hour = int(input("\nEnter amount of hour :\t"))
-        minutes = int(input("Enter amount of minutes :\t"))
-        seconds = int(input("Enter amount of seconds :\t"))
-        total = hour*60*60+minutes*60+seconds
-        print("\nTimer starts now...")
-        time.sleep(1)
-        while total >= 1 :
-            print(total)
-            total -= 1
-            time.sleep(1)
-            if name == "nt" :
-                system("cls")
-            else:
-                system("clear")
-        print("Timer ended...")
-    elif "n" in choice.lower() :
-        print("\nExiting...")
-        break
+
+
+def clear():
+    if name == "nt" :
+        system("cls")
     else:
-        print("\nInvalid input...")
+        system("clear")
+        return name
+    
+
+def total(hour,minutes,seconds):
+    t = hour*60*60+minutes*60+seconds
+    return t
+
+
+def get_time_from_user():
+    hour = int(input("\nEnter amount of hour :\t"))
+    minutes = int(input("Enter amount of minutes :\t"))
+    seconds = int(input("Enter amount of seconds :\t"))
+    t = total(hour,minutes,seconds)
+    return t
+
+
+def output(t):
+    while t >= 1 :
+        print(t)
+        t -= 1
+        time.sleep(1)
+        clear()
+    return t
+
+def main():
+    while True :
+        choice = input("\nDo you want to start? (y/n) :\t")
+        if choice.lower() in ["y","yes"]:
+            t = get_time_from_user()
+            print("\nTimer starts now...")
+            time.sleep(1)
+            output(t)
+            print("Timer ended...")
+        elif choice.lower() in ["n","no"] :
+            print("\nExiting...")
+            break
+        else:
+            print("\nInvalid input...")
+main()

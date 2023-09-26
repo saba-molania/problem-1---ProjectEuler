8d065dfb79867caae2045a76edd5c09546ec5d71
Saba <saba.molania51@gmail.com>
2023-09-26 20:14:50 +0330

    test_sort

--- farzanegan/List/Test_Bubble_Sort.py
+++ farzanegan/List/Test_Bubble_Sort.py
@@ -1,7 +1,8 @@
 
 from bubble_sort import bubble_sort
 def test_bubble_sort():
-    assert bubble_sort([21, 87, 76, 65, 54, 43, 32, 21, 90, 89, 102, 45, 13]) == [13, 21, 21, 32, 43, 45, 54, 65, 76, 87, 89, 90, 102] , "Sould be [13, 21, 21, 32, 43, 45, 54, 65, 76, 87, 89, 90, 102]"
+    _list = [21, 87, 76, 65, 54, 43, 32, 21, 90, 89, 102, 45, 13]
+    assert bubble_sort(_list) == sorted(_list) , "list is not sorted"
 
 if __name__ == "__main__":
     test_bubble_sort()
